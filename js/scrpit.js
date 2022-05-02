2d650aa5a35d3a1bf84edd2b318caf917583a020
João Victor Corado <jhonisilva545@gmail.com>
2022-05-01 22:59:47 -0300

    index

---
+++ js/scrpit.js
@@ -0,0 +1,9 @@
+const ac = document.querySelector("#ac") = ''
+const porcetagem = document.querySelector("#%") = '%'
+const divisao = document.querySelector("#/") = '/'
+const sete = document.querySelector("#7") = '7'
+const oito = document.querySelector("#8") = ''
+
+function resultado(){
+    
+}
